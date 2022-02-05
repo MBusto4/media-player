abb4593627335fbe2ff0ef5956c49852106e95dc
MBusto <mikebusto4@gmail.com>
2022-02-05 11:16:27 -0500

    Full project setup using next.js/tailwindcss

---
+++ media-player-spotify/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h2>hello</h2>
+      <main>
+        {/*SideBar */}
+        {/* Main */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
+    </div>
+  )
+}
