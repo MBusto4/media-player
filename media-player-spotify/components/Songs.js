5daa35ecaffb3702bff33ca6a048c6c1066b4b74
MBusto <mikebusto4@gmail.com>
2022-02-06 15:19:55 -0500

    Feat: created the Song and Songs components

--- media-player-spotify/components/Songs.js
+++ media-player-spotify/components/Songs.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { playListState } from '../atoms/playlistAtom';
+import Song from './Song';
 
 const Songs = () => {
-    return <div></div>;
+    const playlist = useRecoilValue(playListState)
+    return (
+        <div className='flex px-8 flex-col space-y-1 pb-28 text-white'>
+            {playlist?.tracks?.items.map((song, songOrder) => (
+                <Song key={song.track.id} song={song} order={songOrder} />
+            ))}
+        </div>
+    )
 };
 
 export default Songs;
